0ed4fbb9056a47096c46586333d5b46d5260d30d
Snikiwe2108 <119668845+Snikiwe2108@users.noreply.github.com>
2022-12-02 14:08:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout
 def get_data_from_excel():
     df = pd.read_excel(
         io="supermarkt_sales.xlsx",
-        engine="openpyxl",
         sheet_name="Sales",
         skiprows=3,
         usecols="B:R",
