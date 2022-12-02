ef7a363ea14484719bf2af60cfa5d1ad70cb6276
Snikiwe2108 <119668845+Snikiwe2108@users.noreply.github.com>
2022-12-02 19:50:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,15 @@ import streamlit as st  # pip install streamlit
 from streamlit_option_menu import option_menu
 import pip
 pip.main(["install", "openpyxl"])
+
+
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 st.title('Greatness')
-df =  pd.read_excel('supermarkt_sales.xlsx')
+@st.cache
+def get_data_from_excel():
+         df =  pd.read_excel('supermarkt_sales.xlsx',engine="openpyxl",sheet_name="Sales",skiprows=3,usecols="B:R",nrows=1000)
+
 with st.sidebar:
          selected = option_menu("Main Menu", ["Home", "Upload", "Tasks", 'Settings'],icons=['house','cloud-upload', "list-task", 'gear'], menu_icon="cast", default_index=1)
          selected
