7f6162649673120c211a0f53437c54f69f945ef7
Snikiwe2108 <119668845+Snikiwe2108@users.noreply.github.com>
2022-12-12 06:52:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,18 +24,7 @@ with st.sidebar:
          selected
 
          
-left_column, middle_column, right_column = st.columns(3)
-with left_column:
-    st.subheader("Total Sales:")
-    st.subheader(f"US $ {total_sales:,}")
-with middle_column:
-    st.subheader("Average Rating:")
-    st.subheader(f"{average_rating} {star_rating}")
-with right_column:
-    st.subheader("Average Sales Per Transaction:")
-    st.subheader(f"US $ {average_sale_by_transaction}")
 
-st.markdown("""---""")
 
 # ---- SIDEBAR ----
 st.sidebar.header("Please Filter Here:")
@@ -69,6 +58,19 @@ average_rating = round(df_selection["Rating"].mean(), 1)
 star_rating = ":star:" * int(round(average_rating, 0))
 average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
 
+
+left_column, middle_column, right_column = st.columns(3)
+with left_column:
+    st.subheader("Total Sales:")
+    st.subheader(f"US $ {total_sales:,}")
+with middle_column:
+    st.subheader("Average Rating:")
+    st.subheader(f"{average_rating} {star_rating}")
+with right_column:
+    st.subheader("Average Sales Per Transaction:")
+    st.subheader(f"US $ {average_sale_by_transaction}")
+
+st.markdown("""---""")
 
 st.write(df)
 
