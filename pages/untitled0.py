723556dbcfb69a3f5e0b3fb43615aabd0b9573c0
Snikiwe2108 <119668845+Snikiwe2108@users.noreply.github.com>
2022-12-12 08:03:10 +0200

    Update untitled0.py

--- pages/untitled0.py
+++ pages/untitled0.py
@@ -5,3 +5,14 @@ Created on Sun Oct 30 13:14:18 2022
 @author: User
 """
 
+import pandas as pd  # pip install pandas openpyxl
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
+from streamlit_option_menu import option_menu
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+
+df = pd.read_csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv")
+
+
+st.write(df)
